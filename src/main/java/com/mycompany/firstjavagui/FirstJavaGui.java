80874a37c7131c42b4cb57c0e729513c1f7e77af
ST10504417 <ST10504417@myemeris.edu.za>
2026-02-25 09:39:24 +0200

    final GUI

---
+++ src/main/java/com/mycompany/firstjavagui/FirstJavaGui.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstjavagui;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author AKST10504417
+ */
+public class FirstJavaGui {
+
+    public static void main(String[] args) {
+    
+        JOptionPane.showMessageDialog(null, "25°F");
+        
+        JOptionPane.showMessageDialog(null, "Programming is fun!!!", 
+                "Daily reminder", 1);
+        
+        JOptionPane.showMessageDialog(null, "BMWs rock", "BEWARE", 2);
+        
+        JOptionPane.showMessageDialog(null,"Dividing any number by zero is undefined." , 
+                "Be careful", 0);
+    }
+}
